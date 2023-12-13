eccd452d4b247ae4a7382d9aa2edcc29fbfd1e53
rahil karkar <karkarrahil883@gmail.com>
2023-12-13 23:10:22 +0530

    13-12-2023 view setup

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express = require('express');
-
+const path = require('path')
 const app = express();
 const dotenv = require('dotenv')
 
 // configuration for donenv 
 dotenv.config({ path: './.env' })
 
+app.set("view engine", "ejs");
 
-app.listen('8000', () => {
+app.set("views", path.resolve('./views'))
+
+
+app.get("/", (req, res) => {
+    res.render('Home')
+})
+
+
+app.listen(process.env.SERVER_PORT, () => {
     console.log(`port is started on ${process.env.SERVER_PORT}`)
 })
